b041ab52bb4e115c35dd96addd0d06b4e9f340f9
Akash Tomar <akashtomar9456@gmail.com>
2024-07-09 16:22:47 +0530

    adding probablity problem

---
+++ Probablity/1279D.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int MOD = 998244353;
+struct Mint {
+    int val;
+
+    Mint(long long v = 0) {
+        if (v < 0)
+            v = v % MOD + MOD;
+        if (v >= MOD)
+            v %= MOD;
+        val = v;
+    }
+
+    static int mod_inv(int a, int m = MOD) {
+        int g = m, r = a, x = 0, y = 1;
+        while (r != 0) {
+            int q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        }
+        return x < 0 ? x + m : x;
+    }
+    explicit operator int() const {
+        return val;
+    }
+    Mint& operator+=(const Mint &other) {
+        val += other.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    Mint& operator-=(const Mint &other) {
+        val -= other.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+           #if !defined(_WIN32) || defined(_WIN64)
+                return x % m;
+           #endif
+           unsigned x_high = x >> 32, x_low = (unsigned) x;
+           unsigned quot, rem;
+           asm("divl %4\n"
+            : "=a" (quot), "=d" (rem)
+            : "d" (x_high), "a" (x_low), "r" (m));
+           return rem;
+    }
+    Mint& operator*=(const Mint &other) {
+        val = fast_mod((uint64_t) val * other.val);
+        return *this;
+    }
+    Mint& operator/=(const Mint &other) {
+        return *this *= other.inv();
+    }
+    friend Mint operator+(const Mint &a, const Mint &b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint &a, const Mint &b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint &a, const Mint &b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint &a, const Mint &b) { return Mint(a) /= b; }
+    Mint& operator++() {
+        val = val == MOD - 1 ? 0 : val + 1;
+        return *this;
+    }
+    Mint& operator--() {
+        val = val == 0 ? MOD - 1 : val - 1;
+        return *this;
+    }
+    // friend Mint operator<=(const Mint &a, const Mint &b) { return (int)a <= (int)b; }
+    Mint operator++(int32_t) { Mint before = *this; ++*this; return before; }
+    Mint operator--(int32_t) { Mint before = *this; --*this; return before; }
+    Mint operator-() const {
+        return val == 0 ? 0 : MOD - val;
+    }
+    bool operator==(const Mint &other) const { return val == other.val; }
+    bool operator!=(const Mint &other) const { return val != other.val; }
+    Mint inv() const {
+        return mod_inv(val);
+    }
+    Mint power(long long p) const {
+        assert(p >= 0);
+        Mint a = *this, result = 1;
+        while (p > 0) {
+            if (p & 1)
+                result *= a;
+
+            a *= a;
+            p >>= 1;
+        }
+        return result;
+    }
+    friend ostream& operator << (ostream &stream, const Mint &m) {
+        return stream << m.val;
+    }
+    friend istream& operator >> (istream &stream, Mint &m) {
+        return stream>>m.val;
+    }
+};
+int32_t main(){
+    int n;
+    cin>>n;
+    vector<vector<int>>a(n + 1);
+    vector<int>b(1e6 + 1, 0);
+    for(int i = 1; i <= n; i++){
+        int k;
+        cin>>k;
+        while(k--){
+            int x;
+            cin>>x;
+            a[i].push_back(x);
+            b[x]++;
+        }
+
+    }
+    Mint res = 0;
+    for(int i = 1; i <= n; i++){
+        for(auto v: a[i]){
+            res += (Mint)1/(n) * 1 / (a[i].size()) * b[v] / n;
+        }
+    }
+    cout<<res<<endl;
+}
